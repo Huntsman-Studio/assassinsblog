f88e6f6278ca011fb770e62bb144f6d992116777
Kristel Lamaj <kri.lamaj@gmail.com>
2022-12-06 16:11:39 +0200

    blog post 1st approach

--- Client/src/app/modules/modules.module.ts
+++ Client/src/app/modules/modules.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderModule } from './header/header.module';
 import { FooterModule } from './footer/footer.module';
 import { CategoriesComponent } from './categories/categories.component';
+import { SearchComponent } from './search/search.component';
 
 
 
 @NgModule({
   declarations: [
-    CategoriesComponent
+    CategoriesComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
@@ -18,7 +20,8 @@ import { CategoriesComponent } from './categories/categories.component';
   exports: [
     HeaderModule,
     FooterModule,
-    CategoriesComponent
+    CategoriesComponent,
+    SearchComponent
   ]
 })
 export class ModulesModule { }
